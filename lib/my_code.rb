de35fd91e69b7bd8eb2c9339614f06c8d7a81197
Jack Overby <jroverby92@gmail.com>
2020-01-13 23:18:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(array)
+  result = []
+  for ho in array do
+    result.push(yield(ho))
+  end
+  result
+end
+
+def reduce(array, sv = nil)
+  result = (sv)? sv : array[0]
+  for i in ((sv)? 0 : 1)...array.length do
+    result = yield(result, array[i])
+  end
+  result
+end
